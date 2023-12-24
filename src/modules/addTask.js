3b1d058550ea270d076620c2e75ec78c4fe498f8
aciken <adrianmarton2006@gmail.com>
2023-12-24 12:49:17 +0100

    trying to make changing main function

--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -1,8 +1,10 @@
+import { taskArray } from "./createTask";
+
 const addTask = () =>{
 
-    const taskIcon = document.querySelector('.add-task-icon');
     const addTask = document.querySelector('.add-task');
     const main = document.querySelector('.main');
+    const addPart = document.querySelector('.add-part');
 
     
 
@@ -11,7 +13,8 @@ const addTask = () =>{
         addTask.classList.toggle('clicked');
 
         const inputDiv = document.createElement('div');
-        main.appendChild(inputDiv);
+        inputDiv.classList.add('input-todo');
+        addPart.appendChild(inputDiv);
     
     
         const addInput = document.createElement('input');
@@ -27,6 +30,14 @@ const addTask = () =>{
         BtnCreate.classList.add('task-create');
         BtnCreate.textContent = 'Create';
         btnDiv.appendChild(BtnCreate);
+        BtnCreate.addEventListener('click', () =>{
+            if(addInput.value !== ''){
+                toDoCreate(addInput.value)
+                inputDiv.remove();
+                addTask.classList.toggle('clicked');
+            }
+
+        })
     
     
     
@@ -35,7 +46,32 @@ const addTask = () =>{
         BtnCancle.textContent = 'Cancle';
         btnDiv.appendChild(BtnCancle);
 
+        BtnCancle.addEventListener('click', () =>{
+            inputDiv.remove();
+            addTask.classList.toggle('clicked');
+        })
+
     })
+
+
+}
+
+const toDoCreate = (text) =>{
+    const taskShow = document.querySelector('.tasks-show');
+
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('one-task');
+    taskShow.appendChild(taskDiv);
+
+
+
+    const svgString = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle-outline</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
+    taskDiv.innerHTML = svgString;
+
+    const taskText = document.createElement('p');
+    taskText.textContent = text;
+    taskDiv.appendChild(taskText);
+
 }
 
 export default addTask;
