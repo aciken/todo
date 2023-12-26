974e30a2512df9b942369f4f1e5497c36d87ca2e
aciken <adrianmarton2006@gmail.com>
2023-12-26 11:37:56 +0100

    trying to make clicked function

---
+++ src/modules/iconClick.js
@@ -0,0 +1,20 @@
+const iconClick = () => {
+    const taskShow = document.querySelector('.tasks-show');
+
+    taskShow.addEventListener('click', (e) => {
+        if (e.target.classList.contains('circle-icon')) {
+            const parentTask = e.target.closest('.one-task');
+
+            
+
+            if (parentTask) {
+                
+                parentTask.firstChild.innerHTML = '<svg class="circle-icon clicked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle</title><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
+                parentTask.classList.toggle('clicked');
+            }
+        }
+    });
+};
+
+
+export default iconClick
--- src/modules/oneTaskCreate.js
+++ src/modules/oneTaskCreate.js
@@ -1,43 +1,46 @@
-const oneTaskCreate = (array) =>{
+import iconClick from "./iconClick";
 
-const taskShow = document.querySelector('.tasks-show');
-console.log(array)
-
-
-if(taskShow.childElementCount > 0){
-    while (taskShow.firstChild) {
-        taskShow.removeChild(taskShow.firstChild);
-    }
-    
-}
-
-
-for(let i = 0; i < array.length; i++){
-
-    toDoCreate(array[i]);
-}
-
-}
-
-const toDoCreate = (text) =>{
-    
+const oneTaskCreate = (array) => {
     const taskShow = document.querySelector('.tasks-show');
 
-    // taskShow.children.remove();
+    // Clear existing tasks
+    taskShow.innerHTML = '';
+
+    for (let i = 0; i < array.length; i++) {
+        toDoCreate(array[i], i);
+    }
+};
+
+const toDoCreate = (text, index) => {
+    const taskShow = document.querySelector('.tasks-show');
 
     const taskDiv = document.createElement('div');
     taskDiv.classList.add('one-task');
     taskShow.appendChild(taskDiv);
 
+    taskDiv.dataset.index = index;
+    console.log(taskDiv)
 
-
-    const svgString = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle-outline</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
+    const svgString = '<svg class="circle-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle-outline</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
     taskDiv.innerHTML = svgString;
 
     const taskText = document.createElement('p');
     taskText.textContent = text;
     taskDiv.appendChild(taskText);
 
-}
+    const iconDiv = document.createElement('div');
+    iconDiv.classList.add('icon-div');
+    taskDiv.appendChild(iconDiv);
+
+    const svgString1 = '<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>';
+    iconDiv.innerHTML = svgString1;
+
+    const svgString2 = '<svg class="editi-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>square-edit-outline</title><path d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z" /></svg>';
+    iconDiv.innerHTML += svgString2;
+
+    iconClick();
+
+};
+
 
 export default oneTaskCreate
