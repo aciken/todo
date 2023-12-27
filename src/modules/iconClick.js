d0ca1f6f4d7b8aba9076d83874565aab63fe6499
aciken <adrianmarton2006@gmail.com>
2023-12-27 20:11:08 +0100

    delete bug

--- src/modules/iconClick.js
+++ src/modules/iconClick.js
@@ -34,7 +34,7 @@ const deleteIconClick = () =>{
 
     taskShow.addEventListener('click', (e) =>{
         if(e.target.classList.value == 'delete-icon'){
-        taskArray.tasks[arrayNum.taskNum[arrayNum.taskNum.length-1]-1].array.splice(e.target.dataset.index, 1)
+        taskArray.tasks[arrayNum.taskNum[arrayNum.taskNum.length-1]-1].array.splice(e.target.parentElement.parentElement.dataset.index, 1)
         taskArray.tasks[arrayNum.taskNum[arrayNum.taskNum.length-1]-1].state.splice(e.target.dataset.index, 1)
         oneTaskCreate(taskArray.tasks[arrayNum.taskNum[arrayNum.taskNum.length-1]-1].array, arrayNum.taskNum[arrayNum.taskNum.length-1]-1, taskArray.tasks[arrayNum.taskNum[arrayNum.taskNum.length-1]-1].state)
         }
